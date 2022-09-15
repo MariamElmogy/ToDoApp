4cff29f7994d7672b07a70b77e775cdb7b05fd4b
mariam <mariamelmogy3@gmail.com>
2022-09-15 12:49:59 +0200

    created splash screen added no action bar style created home activity create transition to home

--- app/src/main/java/com/example/android11/HomeActivity.kt
+++ app/src/main/java/com/example/android11/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.example.android11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
